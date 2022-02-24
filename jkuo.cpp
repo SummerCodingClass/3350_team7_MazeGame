97b153f5268feb9572c7e101c3bac27d720f60a5
SummerCodingClass <jkuo@sandiego.edu>
2022-02-24 12:31:26 -0800

    lab 5 progress

--- jkuo.cpp
+++ jkuo.cpp
@@ -1,3 +1,30 @@
 //Author: Jennifer K
 
 //Individual Source File for Project
+
+
+
+#include <iostream>
+using namespace std;
+
+// class jk_Test {
+//     private: 
+//         string msg;
+
+//     public:
+//         void PrintName(string msg) 
+//         {
+//             cout << msg;
+//             return;
+//         }
+
+
+// };
+
+
+
+void jk_PrintMsg() 
+{
+    cout << "jk";
+}
+
