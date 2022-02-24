37f588596e109220cb465930a269289503bcf928
Jeff Hicks <jeffreyhicks90@gmail.com>
2022-02-24 11:52:37 -0800

    added function

--- jhicks.cpp
+++ jhicks.cpp
@@ -7,4 +7,13 @@
 using namespace std;
 
 
+class jh_Test {
+    private:
+        string name = "Jeff Hicks";
+    public:
+        void printName (string name)
+        {
+            cout << name << endl;
+        }
 
+}
