5a9e62cf839b202b9906f5f2ff1077ca9576060e
jeffh1cks <jeffreyhicks90@gmail.com>
2022-03-06 15:07:53 -0800

    Updated file

--- jhicks.cpp
+++ jhicks.cpp
@@ -1,12 +1,18 @@
 // Jeff Hicks
-//
 // Source File for Project
 //
 #include <iostream>
 
 using namespace std;
+/*
+ * include in asteroids with header and class with name. 
+ * #include "jhicks.h"
+class Hicks {
+     public:
+         friend void jh_PrintMsg();
 
-
+}
+*/
 void jh_PrintMsg()
 {
     cout << "Hello from Jeff's file!" << endl;
---
+++ jhicks.h
@@ -0,0 +1,14 @@
+#ifndef_jhicks_h_
+#define _jhicks_h_
+// Jeff Hicks
+//
+// Source File for Project
+//
+class Hicks {
+    public:
+    void jh_PrintMsg(); 
+};
+
+#endif
+
+
