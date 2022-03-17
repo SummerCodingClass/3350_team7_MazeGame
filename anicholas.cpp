0ed562d5365e85d1d32e3ca64e621f21ab1ac9b5
Adam Nicholas <anicholas@odin.cs.csubak.edu>
2022-03-17 12:25:50 -0700

    added last name

--- anicholas.cpp
+++ anicholas.cpp
@@ -12,6 +12,6 @@ void an_PrintMsg() {
 
 void an_showCreditPage(Rect position, int defaultHeight, int color) { 
     position.bot = defaultHeight;
-    ggprint8b(&position, 16, color, "Adam: ");
-    ggprint8b(&position, 16, color, "Game Designer, Game Programmer, Sound Designer");
+    ggprint8b(&position, 16, color, "Adam Nicholas : ");
+    ggprint8b(&position, 16, color, "Game Designer | Game Programmer | Sound Designer");
 }
