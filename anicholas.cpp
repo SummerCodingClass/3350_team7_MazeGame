c89f2f8e60f5404dadac652e0959ac41a1fa3c35
Adam Nicholas <anicholas@odin.cs.csubak.edu>
2022-03-17 12:00:28 -0700

    added credit funciton

--- anicholas.cpp
+++ anicholas.cpp
@@ -2,8 +2,15 @@
 // File: Individual Contributions- Group Project
 
 #include <iostream>
+#include "fonts.h"
+
 using namespace std;
 
 void an_PrintMsg() {
     cout << "anicholas file worked!" << endl;
+}
+
+void an_showCreditPage(Rect position, int defaultHeight, int color) { 
+    position.bot = defaultHeight;
+    ggprint8b(&position, 16, color, "Adam: ");
 }
