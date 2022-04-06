b6d403264f872918472060982b15fc88c2f3a7f7
jeffh1cks <jeffreyhicks90@gmail.com>
2022-04-06 09:02:24 -0700

    Changed to K&R styling

--- jhicks.cpp
+++ jhicks.cpp
@@ -7,7 +7,8 @@
 #include "GridCells.h"
 using namespace std;
 
-void jhicksMidtermFunction(bool test) {
+void jhicks_midterm_function(bool test)
+{
     if (test) {
         cout << "Good Work!" << endl;
     } else {
@@ -15,13 +16,15 @@ void jhicksMidtermFunction(bool test) {
     }
 }
 
-void jh_PrintMsg() {
+void jh_PrintMsg()
+{
     cout << "Hello from Jeff's file!" << endl;
     return;
 }
 
 
-void jh_showCreditPage(Rect position, int defaultHeight, int color ) {
+void jh_showCreditPage(Rect position, int defaultHeight, int color )
+{
     position.bot = defaultHeight;
     ggprint8b(&position, 16, color, "Jeff Hicks:");
     ggprint8b(&position, 16, color, "Game Designer | Game Programmer");
@@ -29,29 +32,34 @@ void jh_showCreditPage(Rect position, int defaultHeight, int color ) {
 }
 
 
-void jh_showRulesPage(Rect position, int defaultHeight, int color) {
+void jh_showRulesPage(Rect position, int defaultHeight, int color)
+{
     position.bot = defaultHeight;
     ggprint8b(&position, 16, color, "Rule 2:");
     ggprint8b(&position, 16, color, "Timer will stop once you hit the exit.");
 }
 
 
-void jh_showWelcomePage(Rect position, int defaultHeight, int color) {
+void jh_showWelcomePage(Rect position, int defaultHeight, int color)
+{
     position.bot = defaultHeight;
     ggprint8b(&position, 16, color, "right click to print msgs to console");
 }
 
-int GridCells:: setXcoord(int input) {
+int GridCells:: setXcoord(int input)
+{
     xcoord = input;
     return xcoord;
 }
 
-int GridCells:: setYcoord(int input) {
+int GridCells:: setYcoord(int input)
+{
     ycoord = input;
     return ycoord;
 }
 
-bool GridCells:: setSpace(bool input) {
+bool GridCells:: setSpace(bool input)
+{
     space = input;
     return space;
 }
