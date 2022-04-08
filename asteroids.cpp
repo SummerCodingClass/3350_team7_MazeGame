8cbc935a1742be47227f0709b7c49010d5149616
jeffh1cks <jeffreyhicks90@gmail.com>
2022-04-08 08:44:25 -0700

    updated image class

--- asteroids.cpp
+++ asteroids.cpp
@@ -106,52 +106,59 @@ public:
 } gl;
 
 class Image {
-    public:
-    int width, height;
-    unsigned char *data;
-    Image(const char *fname) {
-        if (fname[0] == '\0')
-            return;
-        int ppmFlag = 0;
-        char name[40];
-        strcpy(name, fname);
-        int slen = strlen(name);
-        char ppmname[80];
-        if (strncmp(name+(slen -4), ".ppm",4) == 0)
-            ppmFlag = 1;
-        if (ppmFlag) {
-            strcpy(ppmname, name);
-        } else {
-            name[slen -4] = '\0';
-            sprintf(ppmname, "%s.ppm", name);
-            char ts[100];
-            sprintf(ts, "convert %s %s", fname, ppmname);
-            system(ts);
-        }
-        FILE *fpi = fopen(ppmname, "r");
-        if (fpi) {
-            char line[200];
-            fgets(line, 200, fpi);
-            fgets(line, 200, fpi);
-            while(line[0] == '#' || strlen(line) < 2)
-                fgets(line, 200, fpi);
-            sscanf(line, "%i %i", &width, &height);
-            fgets(line, 200, fpi);
-            int n = width * height * 3;
-            data = new unsigned char[n];
-            for (int i = 0; i < n; i++) {
-                data[i] = fgetc(fpi);
-            }
-            fclose(fpi);
-        } else {
-            printf("ERROR opening image: %s\n", ppmname);
-            exit(0);
-        }
-        if (!ppmFlag)
-            unlink(ppmname);
-		//~Image() {delete [] data;}
-    }
-} Image img[1] = {"coder.png"};
+public:
+	int width, height;
+	unsigned char *data;
+	~Image() { delete [] data; }
+	Image(const char *fname) {
+		if (fname[0] == '\0')
+			return;
+		//printf("fname **%s**\n", fname);
+		int ppmFlag = 0;
+		char name[40];
+		strcpy(name, fname);
+		int slen = strlen(name);
+		char ppmname[80];
+		if (strncmp(name+(slen-4), ".ppm", 4) == 0)
+			ppmFlag = 1;
+		if (ppmFlag) {
+			strcpy(ppmname, name);
+		} else {
+			name[slen-4] = '\0';
+			//printf("name **%s**\n", name);
+			sprintf(ppmname,"%s.ppm", name);
+			//printf("ppmname **%s**\n", ppmname);
+			char ts[100];
+			//system("convert eball.jpg eball.ppm");
+			sprintf(ts, "convert %s %s", fname, ppmname);
+			system(ts);
+		}
+		//sprintf(ts, "%s", name);
+		FILE *fpi = fopen(ppmname, "r");
+		if (fpi) {
+			char line[200];
+			fgets(line, 200, fpi);
+			fgets(line, 200, fpi);
+			//skip comments and blank lines
+			while (line[0] == '#' || strlen(line) < 2)
+				fgets(line, 200, fpi);
+			sscanf(line, "%i %i", &width, &height);
+			fgets(line, 200, fpi);
+			//get pixel data
+			int n = width * height * 3;			
+			data = new unsigned char[n];			
+			for (int i=0; i<n; i++)
+				data[i] = fgetc(fpi);
+			fclose(fpi);
+		} else {
+			printf("ERROR opening image: %s\n",ppmname);
+			exit(0);
+		}
+		if (!ppmFlag)
+			unlink(ppmname);
+	}
+};
+Image img[1] = {"coder.png"};
 
 
 class Ship {
@@ -457,8 +464,8 @@ void init_opengl(void)
    glBindTexture(GL_TEXTURE_2D, gl.textid);
    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-   glTexImage2D(GL_TEXTURE_2D, 0, 3, img.width, img.height, 0,
-       GL_RGB, GL_UNSIGNED_BYTE, img.data);
+   glTexImage2D(GL_TEXTURE_2D, 0, 3, img[0].width, img[0].height, 0,
+       GL_RGB, GL_UNSIGNED_BYTE, img[0].data);
 	glBindTexture(GL_TEXTURE_2D, 0);
 }
 
