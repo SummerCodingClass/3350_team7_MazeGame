f56b3acab50bfbe70f219748a72bca6eabc6c403
ANicholas2 <anicholas@csub.edu>
2022-04-11 14:59:32 -0700

    added temp exit to maze 10

--- anicholas.cpp
+++ anicholas.cpp
@@ -140,8 +140,8 @@ void an_printMaze10(Rect position, int defaultHeight, int color,
                                                 //          1st number: left to right
                                                 //          2nd number: top to bottom
                                                 //  
-    //int endingPosition[2] = {2, 1};           // 5. replace: 
-    int endingPosition[2] = {9, 9};           // 5. replace: 
+    int endingPosition[2] = {2, 1};           // 5. replace: 
+    //int endingPosition[2] = {9, 9};           // 5. replace: 
                                                 //          same as #4.           
                                                 //
     int wallColor[3] = {102, 50, 168};             // 6. replace:
