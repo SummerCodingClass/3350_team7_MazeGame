28167dae896cec0367d87fd5763ded2cd9ad8009
ANicholas2 <anicholas@csub.edu>
2022-04-11 14:21:24 -0700

    added maze 10 placeholder

--- anicholas.cpp
+++ anicholas.cpp
@@ -126,3 +126,79 @@ void an_printMaze9(Rect position, int defaultHeight, int color,
                                                 color, mazeName, endReached);
     }
 }
+
+void an_printMaze10(Rect position, int defaultHeight, int color, 
+            int (&player)[2], bool &firstRun, bool& endReached, Grid& mazeGrid, 
+                                                                int& maze_state)
+{
+    const char* mazeName = "Maze 10";            // 2. replace
+    int rows = 15;                              // 3. replace: 
+                                                //          start counting from 1
+    int startingPosition[2] = {1, 1};          // 4. replace: 
+                                                //          start counting from 0.
+                                                //          1st number: left to right
+                                                //          2nd number: top to bottom
+                                                //  
+    int endingPosition[2] = {5, 13};           // 5. replace: 
+                                                //          same as #4.           
+                                                //
+    int wallColor[3] = {102, 50, 168};             // 6. replace:
+                                                //          this is rgb color for wall. 
+                                                //          don't forget #7
+
+    // source: https://www.asciiart.eu/art-and-design/mazes
+    // corridors need to be 1 space wide, or it will mess up the trail
+ 
+
+    // 7. replace
+    const char* maze[rows] = 
+    {
+
+        ".-.-.-.-.-.",
+        "|   |     |",
+        ": :-: : :-:",
+        "|     | | |",
+        ":-:-:-: : :",
+        "|       | |",
+        ": : :-: : :",
+        "| |   |   |",
+        ": :-:-:-: :",
+        "| |     | |",
+        ": : :-: : :",
+        "|   |   | |",
+        ": :-: :-: :",
+        "|   | |   |",
+        ":-:-:-:-:-:",
+    
+    };
+
+    int columns = getColumns(maze, rows);
+
+    if (firstRun) {
+        player[0] = startingPosition[0]; // x
+        player[1] = startingPosition[1]; // y
+
+        mazeGrid = Grid(maze, rows, columns, player, endingPosition, 
+                                                                wallColor);
+   
+        mazeGrid.printGrid(position, rows, columns, player, defaultHeight, 
+                                                color, mazeName, endReached);
+
+        firstRun = false;
+    } else if (endReached) {
+        cout << "end reached" << endl; 
+        mazeGrid.printGrid(position, rows, columns, player, defaultHeight, 
+                                                color, mazeName, endReached);
+
+        
+        maze_state = -1 * maze_state;
+        firstRun = true;
+        endReached = false;
+        
+
+    } else {
+       
+        mazeGrid.printGrid(position, rows, columns, player, defaultHeight, 
+                                                color, mazeName, endReached);
+    }
+}
