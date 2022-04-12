365f7559039289a637492c7a82f2a339d29af772
SummerCodingClass <jkuo@sandiego.edu>
2022-04-11 17:14:43 -0700

    start of refactor 1 - both .h files - keeping comment ver.

--- jkuo.cpp
+++ jkuo.cpp
@@ -1175,8 +1175,31 @@ int GridCells::getYcoord()
     return ycoord;
 }
 
+GridCells::GridCells() {
+    xcoord = 0;
+    ycoord = 0;
+    space = true;
+    wall = true;
+    start = true;
+    end = true;
+    currentPosition = true;
+    traveled = true;
+}
+
+GridCells::GridCells(int x, int y, bool uspace, bool uwall, 
+                bool ustart, bool uend, bool ucurrent, bool utravel) {
+
+    xcoord = x;
+    ycoord = y;
+    space = uspace;
+    wall = uwall;
+    start = ustart;
+    end = uend;
+    currentPosition = ucurrent;
+    traveled = utravel;
 
 
+}
 
 
 
